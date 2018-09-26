8c4ea9a992a6acf6d2a04203c0a558e02001d9a0
justina1224 <justyna.pilzno@gmail.com>
2018-09-26 15:10:25 +0200

    tasks crud v2

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -18,10 +18,10 @@ public class TasksApplication /*extends SpringBootServletInitializer*/ {
 	public static void main(String[] args) {
 		SpringApplication.run(TasksApplication.class, args);
 	}
-	/*
+/*
 	@Override
 	protected SpringApplicationBuilder configure (SpringApplicationBuilder application) {
 		return application.sources(TasksApplication.class);
 	}
-	*/
+*/
 }
